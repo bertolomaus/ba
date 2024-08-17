eab578be3bafa3a2bcbb89d41bde386261d8b91e
Felix Mangold <felix.mangold@student.uni-luebeck.de>
2024-08-17 15:24:14 +0200

    added navigation component

---
+++ components/Navigation.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <nav class="navigation">
+    <ul class="flex flex-row justify-center">
+      <li><NuxtLink to="/">Dashboard</NuxtLink></li>
+      <li><NuxtLink to="/profil">Profil</NuxtLink></li>
+    </ul>
+  </nav>
+</template>

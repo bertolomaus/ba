6abbd4b93a8bda677dcf93a601e6118ecd95baf1
Felix Mangold <felix.mangold@student.uni-luebeck.de>
2024-08-18 17:11:00 +0200

    added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,40 @@ export default {
       sans: ["Roboto", "sans-serif"],
       serif: ["Roboto Mono", "serif"],
     },
+    extend: {
+      primary: {
+        DEFAULT: '#00112f',
+        light: '#192843',
+        dark: '#000b20'
+      },
+      secondary: {
+        DEFAULT: '#eb4747'
+      },
+      highlight: {
+        DEFAULT: '#ffd824'
+      },
+      muted: {
+        DEFAULT: '#7f8897'
+      },
+      inverted: {
+        DEFAULT: '#e5e7ea',
+        primary: '#e5e7ea',
+        secondary: '#e5e7ea',
+        highlight: '#ffffff',
+        text: '#cccfd5',
+        muted: '#192843',
+        mutedSecondary: '#ef6b6b'
+      },
+      success: {
+        DEFAULT: '#33ff88'
+      },
+      error: {
+        DEFAULT: '#eb4747'
+      },
+      warning: {
+        DEFAULT: '#ffd824'
+      }
+    }
   },
   plugins: [],
 };
